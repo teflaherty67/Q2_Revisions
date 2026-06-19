e28a4ed17ee25615d336ddc3e92400b60b4e21df
Tom E Flaherty <67797311+teflaherty67@users.noreply.github.com>
2026-06-19 09:06:27 -0500

    Add project files.

---
+++ Q2_Revisions/Common/CommandAvailability.cs
@@ -0,0 +1,17 @@
+﻿namespace Q2_Revisions.Common
+{
+    internal class CommandAvailability : IExternalCommandAvailability
+    {
+        public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
+        {
+            bool result = false;
+            UIDocument activeDoc = applicationData.ActiveUIDocument;
+            if (activeDoc != null && activeDoc.Document != null)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Q2_Revisions/cmdQ2Revs.cs
@@ -0,0 +1,18 @@
+﻿namespace Q2_Revisions
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdQ2Revs : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Revit application and document variables
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            // Your code goes here
+
+            return Result.Succeeded;
+        }       
+    }
+}
